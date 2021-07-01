937f699ef6b01de54355dd3809c4128dbfa4695d
artemvel <83668965+artemvel@users.noreply.github.com>
2021-06-30 21:27:16 -0400

    User story102 abraham (#2)
    * User Story 102 has started
    
    * User Story 102 has finished

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,6 @@
+public class UserStory102 {
+    //User Story 102 started
+    //ready to push this branch to remote
+    //and open the pull request so I can start collaborating
+    //so my team member can comment, review and approve or request
+}
